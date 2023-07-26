ed6c124e7249d84733c3e0d11f24d86979aae847
Eleonora Nocentini Sköldebrink <eleonora.nocentini@gmail.com>
2023-07-26 19:59:43 +0200

    2023-07-26

--- src/Message.tsx
+++ src/Message.tsx
@@ -1,7 +1,10 @@
 
-// Pascal Cascading
+// PascalCasing
 function Message() {
     //JSX :Javascript XML
+    const name = 'Elli';
+    if (name)
+        return <h1>Hello {name}</h1>;
     return <h1>Hello World</h1>;
 }
 
